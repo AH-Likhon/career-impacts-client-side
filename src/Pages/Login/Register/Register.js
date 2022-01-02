2f7e9ace733321049e68f35589927b802ea19511
ihasanarnob <hasanarnob3@gmail.com>
2022-01-02 21:01:20 +0600

    imported firebase auth and about page

---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Form, Button, Nav, Spinner, Alert } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
+
+const Register = () => {
+    const [loginData, setLoginData] = useState({});
+    const navigate = useNavigate();
+    const { user, authError, registerUser, isLoading } = useAuth();
+
+    const handleOnBlur = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+
+        console.log(newLoginData);
+    }
+
+    const handleRegister = (e) => {
+        if (loginData.password !== loginData.password1) {
+            alert('Please enter your right password');
+            return;
+        }
+        registerUser(loginData.email, loginData.password, loginData.name, navigate);
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <h3 className="d-flex justify-content-center mt-5">Please Sign Up Here</h3>
+            {!isLoading && <Form onSubmit={handleRegister} className=" w-50 container container-fluid my-4">
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>User Name </Form.Label>
+                    <Form.Control onBlur={handleOnBlur} name="name" type="text" placeholder="Enter name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onBlur={handleOnBlur} name="email" type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onBlur={handleOnBlur} name="password" type="password" placeholder="Enter Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control onBlur={handleOnBlur} name="password1" type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <Button className="w-25 p-2" variant="primary" type="submit">
+                    Sign Up
+                </Button>
+                {/* Go to Sign Up Page*/}
+                <Nav.Link as={Link} to="/login" className="fw-bold m-3">Already Signed Up? Please Log In</Nav.Link>
+            </Form>}
+            {isLoading && <Spinner animation="border" />}
+
+            {user?.email && <Alert variant="success">
+                User signed up successfully!
+            </Alert>}
+            {
+                authError && <Alert variant="danger">
+                    {authError.message || authError}
+                </Alert>
+            }
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,140 @@
+import { useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import initializeAuthentication from '../Pages/Login/Firebase/Firebase.init';
+
+
+// initialize firebase app
+initializeAuthentication();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [authError, setAuthError] = useState('');
+    const [admin, setAdmin] = useState(false);
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const registerUser = (email, password, name, navigate,) => {
+        setIsLoading(true);
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setAuthError('');
+                const newUser = { email, displayName: name, };
+                setUser(newUser);
+
+
+                // save data to database
+                // saveUser(email, name, 'POST', password,);
+
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                    // Profile updated!
+                    // ...
+                }).catch((error) => {
+                    // An error occurred
+                    // ...
+                });
+
+                navigate('/');
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+                // console.log(error);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    const loginUser = (email, password, location, navigate) => {
+        setIsLoading(true);
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // const user = userCredential.user;
+                const destination = location?.state?.from || '/';
+                navigate(destination);
+                setAuthError('');
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    const signInWithGoogle = (location, navigate) => {
+        setIsLoading(true);
+
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const user = result.user;
+
+                // saveUser(user.email, user.displayName, 'PUT');
+
+                const destination = location?.state?.from || '/';
+                navigate(destination);
+                setAuthError('');
+
+            }).catch((error) => {
+                setAuthError(error.message);
+            }).finally(() => setIsLoading(false));
+    }
+
+    // observe user state
+    useEffect(() => {
+        const unSubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+
+            } else {
+                setUser({});
+            }
+            setIsLoading(false);
+        });
+
+        return () => unSubscribed;
+    }, [])
+
+    // useEffect(() => {
+    //     fetch(`https://fierce-island-20603.herokuapp.com/users/${user.email}`)
+    //         .then(res => res.json())
+    //         .then(data => setAdmin(data.admin))
+    // }, [user.email])
+
+    const logOut = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        })
+            .finally(() => setIsLoading(false));
+    }
+
+    // const saveUser = (email, displayName, method) => {
+    //     const user = { email, displayName };
+
+    //     fetch('https://fierce-island-20603.herokuapp.com/users', {
+    //         method: method,
+    //         headers: {
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(user)
+    //     })
+    //         .then()
+    // }
+
+    return {
+        user,
+        admin,
+        registerUser,
+        loginUser,
+        signInWithGoogle,
+        logOut,
+        isLoading,
+        authError
+    }
+}
+
+export default useFirebase;
