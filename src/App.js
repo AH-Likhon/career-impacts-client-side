b9583a4d4ad7a97002e88d9a6a52be841701bb53
Md. Akramul Hoque <ictianlikhon6@gmail.com>
2022-01-02 23:50:25 +0600

    auth

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/HomeComponent/Home/Home';
 import JobDetails from './Pages/HomeComponent/JobFeatures/JobDetails';
+import AuthProvider from '../src/contexts/AuthProvider';
 
 function App() {
   return (
-    <div>
+
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home></Home>}>
@@ -14,7 +16,8 @@ function App() {
           <Route path="home" element={<Home></Home>}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
+    </AuthProvider>
+
   );
 }
 
