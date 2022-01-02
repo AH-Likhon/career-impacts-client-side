9c18afa580dd355f1ac4e2880eb7bced7ef26910
Md. Akramul Hoque <ictianlikhon6@gmail.com>
2022-01-03 01:26:44 +0600

    auth problem solved

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/HomeComponent/Home/Home';
 import JobDetails from './Pages/HomeComponent/JobFeatures/JobDetails';
-import AuthProvider from '../src/contexts/AuthProvider';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
 
 function App() {
   return (
 
-    // <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home></Home>}>
-          <Route path="jobDetails" element={<JobDetails></JobDetails>}></Route>
-        </Route>
-        <Route path="home" element={<Home></Home>}></Route>
-      </Routes>
-    </BrowserRouter>
-    // </AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+            <Route path="jobDetails" element={<JobDetails></JobDetails>}></Route>
+          </Route>
+          <Route path="home" element={<Home></Home>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
 
   );
 }
