b0d1548164c1817107c2ef8e14f0e47dcf34cb18
Munni2244 <munnikhatun51544@gmail.com>
2022-01-03 00:06:04 +0600

    add jobs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './Pages/Home/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/HomeComponent/Home/Home';
+import JobDetails from './Pages/HomeComponent/JobFeatures/JobDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+      <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Home></Home>}>
+        <Route path="jobDetails" element={<JobDetails></JobDetails>}></Route> 
+       </Route>
+       <Route path="home" element={<Home></Home>}></Route>
+     </Routes>
+     </BrowserRouter>
+  
   );
 }
 
--- src/Pages/Jobs/Jobs.js
+++ src/Pages/Jobs/Jobs.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Jobs = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <h1>This is job page</h1>
         </div>
     );
 };
 
-export default Home;
+export default Jobs;
